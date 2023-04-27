75a98c4206557ba146b4d9ca77305c55cb084b98
Yan Kurbatov <deknowny@gmail.com>
2023-04-27 17:14:33 +0300

    Add contract data, provide real token address

---
+++ src/data/contracts.js
@@ -0,0 +1,17 @@
+import AIFLOPPAToken from "../abi/AIFLOPPAToken.abi.json";
+import AIFLOPPABonusPool from "../abi/AIFLOPPABonusPool.abi.json";
+import DistributionPool from "../abi/DistributionPool.abi.json";
+
+
+export const AIFLOPPATokenContract = {
+    address: "0xa70C1720c7Fcb5741BC81a45D3601923000098Db",
+    abi: AIFLOPPAToken
+}
+export const AIFLOPPABonusPoolContract = {
+    address: "0x755De68aAD215Be9132876a2D5B0363C6b671618",
+    abi: AIFLOPPABonusPool
+}
+export const DistributionPoolContract = {
+    address: "0xC1054Cdc00A374B0af435607b6342Ef52EFc25F6",
+    abi: DistributionPool
+}
