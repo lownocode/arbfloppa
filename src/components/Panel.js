b356a79cae9ac50866b77bfb1e1795d457c91e70
Localhostov <localhostov@gmail.com>
2023-04-28 19:11:41 +0300

    the improve for monitors with large-width

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -3,15 +3,25 @@ import { Header, Footer } from "./index"
 
 export const Panel = ({ children }) => {
     return (
-        <div style={{ padding: "var(--panel-padding)" }}>
+        <>
             <Header />
 
             <div style={{ marginTop: "var(--header-height)" }} />
-            <div style={{ minHeight: "100vh" }}>
-                {children}
+
+            <div
+                style={{
+                    padding: "var(--panel-padding)",
+                    minHeight: "100vh",
+                    display: "flex",
+                    justifyContent: "center",
+                }}
+            >
+                <div style={{ maxWidth: 1280, flex: 1 }}>
+                    {children}
+                </div>
             </div>
 
             <Footer />
-        </div>
+        </>
     )
 }
