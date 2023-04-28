42fce5e8864928ab1430a935adc6f1ba9a60f290
Localhostov <localhostov@gmail.com>
2023-04-28 19:12:04 +0300

    - useless icons + telegram icon

--- src/data/socialNetworks.js
+++ src/data/socialNetworks.js
@@ -1,22 +1,17 @@
 import React from "react"
 
-import { Discord, Medium, Twitter } from "../assets/icons"
+import { Discord, Telegram } from "../assets/icons"
 import { config } from "./config"
 
 export const socialNetworks = [
-    {
-        color: "#1D9BF0",
-        icon: <Twitter />,
-        link: config.links.twitter
-    },
     {
         color: "#5865F2",
         icon: <Discord size={20} />,
         link: config.links.discord
     },
     {
-        color: "#000",
-        icon: <Medium />,
-        link: config.links.medium
-    }
+        color: "#2AABEE",
+        icon: <Telegram size={20} />,
+        link: config.links.telegram
+    },
 ]
