6704e837ee60a1ab938a17bea7c5bc5c635187f5
Localhostov <localhostov@gmail.com>
2023-04-28 19:02:54 +0300

    + telegram link - useless links

--- src/data/config.js
+++ src/data/config.js
@@ -3,9 +3,8 @@ export const config = {
     footerProjectName: "Arbitrum AI FLOPPA",
     contract: "0xa70C1720c7Fcb5741BC81a45D3601923000098Db",
     links: {
-        twitter: "https://twitter.com",
+        telegram: "https://twitter.com",
         discord: "https://discord.com",
-        medium: "https://medium.com",
         luckyDropRules: "https://example.com",
         buyToken: "https://app.camelot.exchange/?token1=0x82aF49447D8a07e3bd95BD0d56f35241523fBab1&token2=0x09E18590E8f76b6Cf471b3cd75fE1A1a9D2B2c2b"
     },
