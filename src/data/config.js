45bbeee8354f93400990a1db4d647de51ebf9424
Localhostov <localhostov@gmail.com>
2023-05-04 22:17:42 +0300

    + new link

--- src/data/config.js
+++ src/data/config.js
@@ -6,7 +6,8 @@ export const config = {
         telegram: "https://twitter.com",
         discord: "https://discord.com",
         luckyDropRules: "https://example.com",
-        buyToken: "https://app.camelot.exchange/?token1=0x82aF49447D8a07e3bd95BD0d56f35241523fBab1&token2=0x09E18590E8f76b6Cf471b3cd75fE1A1a9D2B2c2b"
+        buyToken: "https://app.camelot.exchange/?token1=0x82aF49447D8a07e3bd95BD0d56f35241523fBab1&token2=0x09E18590E8f76b6Cf471b3cd75fE1A1a9D2B2c2b",
+        lpProcessingURL: "https://example.com"
     },
     tokenName: "AIFLOPPA",
     walletConnectProjectId: "f8f117d32eb6c0129e7f638aeda98c31"
